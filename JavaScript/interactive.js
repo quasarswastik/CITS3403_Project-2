db8f67958645aea95627645005400880342aeec5
quasarswastik@gmail.com <quasarswastik@gmail.com>
2020-05-05 11:59:47 +0800

    First batch cahnge on files qsets and author

--- JavaScript/interactive.js
+++ JavaScript/interactive.js
@@ -4,3 +4,11 @@ function showtest() {
     document.getElementById("question_set_title").hidden = false;
     document.getElementById("question_set").hidden = false;
 }
+
+function showresult() {
+    document.getElementById("result_set").hidden = false;
+}
+
+function showAdminResult() {
+    document.getElementById("admin_result_set").hidden = false;
+}
