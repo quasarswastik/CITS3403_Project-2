20ca1201ce34227d18b3756729749e0b35db66d1
Craig Huggins <craig.huggins777@gmail.com>
2020-05-12 19:18:11 +0800

    added User Registration

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+# from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    administrator_check = BooleanField('Sign up as an Administrator')
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email    = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_2 = PasswordField('Password Repeated', validators=[DataRequired(), EqualTo('password')])
+    administrator = BooleanField('Register as an Administrator')
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -40,7 +41,21 @@ def take_test():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, administrator_check={}, remember_me={}'.format(
-            form.username.data, form.administrator_check.data, form.remember_me.data))
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title = 'Log In', form = form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # user = User(username = form.username.data, email=form.email.data)
+        # user.set_password(form.password.data)
+        # db.session.add(user)
+        # db.session.commit()
+        flash('Thanks {}, you have been registered as a New User and may now Log In. administrator={}'.format(
+            form.username.data, form.administrator.data))
+        return redirect(url_for('login'))
+    return render_template('register.html', title = 'Register', form = form)
