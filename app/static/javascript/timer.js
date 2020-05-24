6bec2662979882fab2b7eacef8c190d87ed0243b
quasarswastik@gmail.com <quasarswastik@gmail.com>
2020-05-24 15:51:16 +0800

    trial

---
+++ app/static/javascript/timer.js
@@ -0,0 +1,28 @@
+// var timer;
+// var timerStart;
+
+// function startCounting()    {
+//     alert("Started");
+//     console.log("Reached here!");
+//     var timerStart = new Date().getTime();
+//     var timer = new Date().getTime() - timerStart;
+//     document.getElementById("timer").innerHTML = "Time Elapsed = " + timer * 1000;
+//     var t = setTimeout(startTime, 500);
+// }
+
+// // function startTime() {
+// //     var today = new Date();
+// //     var h = today.getHours();
+// //     var m = today.getMinutes();
+// //     var s = today.getSeconds();
+// //     m = checkTime(m);
+// //     s = checkTime(s);
+// //     document.getElementById('txt').innerHTML =
+// //     h + ":" + m + ":" + s;
+// //     var t = setTimeout(startTime, 500);
+// //   }
+// //   function checkTime(i) {
+// //     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+// //     return i;
+// //   }
+  
